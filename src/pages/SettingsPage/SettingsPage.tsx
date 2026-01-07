7b2be05e36a3f7fec69febcb004cacb95a5e6290
icmx <32983315+icmx@users.noreply.github.com>
2026-01-07 12:35:15 +0600

    add fraction digits setting

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,11 +1,44 @@
-import type { FunctionComponent } from 'react';
+import { useEffect, useState, type FunctionComponent } from 'react';
+import { useFractionDigits } from '../../hooks/useFractionDigitsStore';
 
 export const SettingsPage: FunctionComponent = () => {
+  const [fractionDigits, setFractionDigits] = useFractionDigits();
+  const [resultDigit, setReultDigit] = useState('');
+
+  useEffect(() => {
+    setReultDigit(
+      'Will show: ' + (500.12345678).toFixed(fractionDigits)
+    );
+  }, [fractionDigits]);
+
   return (
     <>
       <title>floats - Settings</title>
 
       <p>Settings Page</p>
+
+      <form>
+        <output style={{ fontFamily: 'monospace' }}>
+          {resultDigit}
+        </output>
+        <div>
+          <label htmlFor="fraction-gigit">Fraction Digits</label>
+          <input
+            id="fraction-gigits"
+            type="number"
+            min={0}
+            max={8}
+            step={1}
+            value={fractionDigits}
+            onChange={(event) => {
+              const text = event.target.value;
+              const int = Number.parseInt(text, 10);
+
+              setFractionDigits(int);
+            }}
+          />
+        </div>
+      </form>
     </>
   );
 };
