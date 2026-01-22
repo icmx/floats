7f2ef7703169d270fce77f5995f0751e4c943fb7
icmx <32983315+icmx@users.noreply.github.com>
2026-01-22 23:26:01 +0600

    split stock chart into separate chunk

--- src/components/currency/Plotter/Plotter.tsx
+++ src/components/currency/Plotter/Plotter.tsx
@@ -1,7 +1,9 @@
-import StockChart from '@highcharts/react/stock';
-import type { FunctionComponent } from 'react';
+import { lazy, Suspense, type FunctionComponent } from 'react';
 import { useFractionDigits } from '../../../hooks/useFractionDigitsStore';
+import { Loading } from '../../common/Loading';
 import type { PlotterProps } from './Plotter.types';
+
+const StockChart = lazy(() => import('@highcharts/react/stock'));
 
 // @todo: move to CSS
 const CHART_COLORS = [
@@ -19,102 +21,104 @@ export const Plotter: FunctionComponent<PlotterProps> = ({
   const [fractionDigits] = useFractionDigits();
 
   return (
-    <StockChart
-      options={{
-        chart: {
-          style: {
-            color: 'var(--color-text)',
-          },
-          backgroundColor: 'var(--color-page)',
-        },
-        xAxis: {
-          labels: {
+    <Suspense fallback={<Loading />}>
+      <StockChart
+        options={{
+          chart: {
             style: {
               color: 'var(--color-text)',
             },
+            backgroundColor: 'var(--color-page)',
           },
-          gridLineColor: 'var(--color-line)',
-        },
-        yAxis: {
-          labels: {
+          xAxis: {
+            labels: {
+              style: {
+                color: 'var(--color-text)',
+              },
+            },
+            gridLineColor: 'var(--color-line)',
+          },
+          yAxis: {
+            labels: {
+              style: {
+                color: 'var(--color-text)',
+              },
+            },
+            gridLineColor: 'var(--color-line)',
+          },
+          tooltip: {
             style: {
               color: 'var(--color-text)',
             },
+            backgroundColor: 'var(--color-chip)',
           },
-          gridLineColor: 'var(--color-line)',
-        },
-        tooltip: {
-          style: {
-            color: 'var(--color-text)',
+
+          rangeSelector: {
+            buttons: [
+              {
+                type: 'month',
+                count: 1,
+              },
+              {
+                type: 'month',
+                count: 3,
+              },
+              {
+                type: 'month',
+                count: 6,
+              },
+              {
+                type: 'ytd',
+                count: 1,
+              },
+              {
+                type: 'year',
+                count: 1,
+              },
+              {
+                type: 'year',
+                count: 5,
+              },
+              {
+                type: 'year',
+                count: 10,
+              },
+              {
+                type: 'all',
+                text: 'All',
+              },
+            ],
           },
-          backgroundColor: 'var(--color-chip)',
-        },
 
-        rangeSelector: {
-          buttons: [
-            {
-              type: 'month',
-              count: 1,
-            },
-            {
-              type: 'month',
-              count: 3,
-            },
-            {
-              type: 'month',
-              count: 6,
-            },
-            {
-              type: 'ytd',
-              count: 1,
-            },
-            {
-              type: 'year',
-              count: 1,
-            },
-            {
-              type: 'year',
-              count: 5,
-            },
-            {
-              type: 'year',
-              count: 10,
-            },
-            {
-              type: 'all',
-              text: 'All',
-            },
-          ],
-        },
-
-        accessibility: {
-          enabled: false,
-        },
-        credits: {
-          enabled: false,
-        },
-        series: series.map((value, index) => {
-          const isSingleSeries = series.length === 1;
-          const type = isSingleSeries ? 'area' : 'line';
-          const color = CHART_COLORS[index];
-
-          return {
-            ...value,
-            type,
-            color,
-            animation: true,
-            tooltip: {
-              valueDecimals: fractionDigits,
-              pointFormat: '{series.name}: {point.y}',
-            },
-          };
-        }),
-        plotOptions: {
-          area: {
-            threshold: null,
+          accessibility: {
+            enabled: false,
           },
-        },
-      }}
-    />
+          credits: {
+            enabled: false,
+          },
+          series: series.map((value, index) => {
+            const isSingleSeries = series.length === 1;
+            const type = isSingleSeries ? 'area' : 'line';
+            const color = CHART_COLORS[index];
+
+            return {
+              ...value,
+              type,
+              color,
+              animation: true,
+              tooltip: {
+                valueDecimals: fractionDigits,
+                pointFormat: '{series.name}: {point.y}',
+              },
+            };
+          }),
+          plotOptions: {
+            area: {
+              threshold: null,
+            },
+          },
+        }}
+      />
+    </Suspense>
   );
 };
