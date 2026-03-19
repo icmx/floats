3d70f7ac22fa94090c4cd6ec530e18daacf60898
icmx <32983315+icmx@users.noreply.github.com>
2026-03-19 17:48:32 +0600

    fix layout

--- src/components/currency/DataChart/DataChart.tsx
+++ src/components/currency/DataChart/DataChart.tsx
@@ -9,7 +9,6 @@ import HighchartsReact from 'highcharts-react-official';
 import { EXPLORE_FRACTION_DIGITS } from '../../../constants/common';
 import { getSeriesColor } from '../../../utils/currency';
 import type { DataChartProps } from './DataChart.types';
-import styles from './DataChart.module.css';
 
 const MS_3_MONTHS = 7516800000;
 
@@ -62,133 +61,131 @@ export const DataChart: FunctionComponent<DataChartProps> = ({
   const isSingleSeries = series.length === 1;
 
   return (
-    <div className={styles.DataChartContainer}>
-      <HighchartsReact
-        ref={chartRef}
-        highcharts={Highcharts}
-        constructorType={'stockChart'}
-        containerProps={{
+    <HighchartsReact
+      ref={chartRef}
+      highcharts={Highcharts}
+      constructorType={'stockChart'}
+      containerProps={{
+        style: {
+          height: '65vh',
+          width: '100%',
+        },
+      }}
+      options={{
+        chart: {
           style: {
-            height: '100%',
-            width: '100%',
+            color: 'var(--color-text)',
           },
-        }}
-        options={{
-          chart: {
+          backgroundColor: 'var(--color-body)',
+        },
+        xAxis: {
+          labels: {
             style: {
               color: 'var(--color-text)',
             },
-            backgroundColor: 'var(--color-body)',
           },
-          xAxis: {
-            labels: {
-              style: {
-                color: 'var(--color-text)',
-              },
-            },
-            gridLineColor: 'var(--color-line)',
-          },
-          yAxis: {
-            labels: {
-              style: {
-                color: 'var(--color-text)',
-              },
-            },
-            gridLineColor: 'var(--color-line)',
-          },
-          tooltip: {
+          gridLineColor: 'var(--color-line)',
+        },
+        yAxis: {
+          labels: {
             style: {
               color: 'var(--color-text)',
             },
-            backgroundColor: 'var(--color-chip)',
           },
+          gridLineColor: 'var(--color-line)',
+        },
+        tooltip: {
+          style: {
+            color: 'var(--color-text)',
+          },
+          backgroundColor: 'var(--color-chip)',
+        },
 
-          rangeSelector: {
-            inputStyle: {
-              color: 'var(--color-link)',
+        rangeSelector: {
+          inputStyle: {
+            color: 'var(--color-link)',
+          },
+          selected: 1,
+          buttons: [
+            {
+              type: 'month',
+              count: 1,
+              text: '1m',
             },
-            selected: 1,
-            buttons: [
-              {
-                type: 'month',
-                count: 1,
-                text: '1m',
-              },
-              {
-                type: 'month',
-                count: 3,
-                text: '3m',
-              },
-              {
-                type: 'month',
-                count: 6,
-                text: '6m',
-              },
-              {
-                type: 'ytd',
-                count: 1,
-                text: 'YTD',
-              },
-              {
-                type: 'year',
-                count: 1,
-                text: '1Y',
-              },
-              {
-                type: 'year',
-                count: 5,
-                text: '5Y',
-              },
-              {
-                type: 'year',
-                count: 10,
-                text: '10Y',
-              },
-              {
-                type: 'all',
-                text: 'All',
-              },
-            ],
-          },
-
-          accessibility: {
-            enabled: false,
-          },
-          credits: {
-            enabled: false,
-          },
-          legend: {
-            itemStyle: {
-              color: 'var(--color-text)',
+            {
+              type: 'month',
+              count: 3,
+              text: '3m',
             },
-
-            enabled: !isSingleSeries,
-            verticalAlign: 'top',
-            layout: 'horizontal',
-            align: 'center',
-          },
-          series: series.map((value, index) => {
-            const type = isSingleSeries ? 'area' : 'line';
-            const color = getSeriesColor(index);
-
-            return {
-              ...value,
-              type,
-              color,
-              animation: true,
-              tooltip: {
-                valueDecimals: EXPLORE_FRACTION_DIGITS,
-                pointFormat: '{series.name}: {point.y}',
-              },
-            };
-          }),
-          plotOptions: {
-            area: {
-              threshold: null,
+            {
+              type: 'month',
+              count: 6,
+              text: '6m',
             },
+            {
+              type: 'ytd',
+              count: 1,
+              text: 'YTD',
+            },
+            {
+              type: 'year',
+              count: 1,
+              text: '1Y',
+            },
+            {
+              type: 'year',
+              count: 5,
+              text: '5Y',
+            },
+            {
+              type: 'year',
+              count: 10,
+              text: '10Y',
+            },
+            {
+              type: 'all',
+              text: 'All',
+            },
+          ],
+        },
+
+        accessibility: {
+          enabled: false,
+        },
+        credits: {
+          enabled: false,
+        },
+        legend: {
+          itemStyle: {
+            color: 'var(--color-text)',
           },
-        }}
-      />
-    </div>
+
+          enabled: !isSingleSeries,
+          verticalAlign: 'top',
+          layout: 'horizontal',
+          align: 'center',
+        },
+        series: series.map((value, index) => {
+          const type = isSingleSeries ? 'area' : 'line';
+          const color = getSeriesColor(index);
+
+          return {
+            ...value,
+            type,
+            color,
+            animation: true,
+            tooltip: {
+              valueDecimals: EXPLORE_FRACTION_DIGITS,
+              pointFormat: '{series.name}: {point.y}',
+            },
+          };
+        }),
+        plotOptions: {
+          area: {
+            threshold: null,
+          },
+        },
+      }}
+    />
   );
 };
