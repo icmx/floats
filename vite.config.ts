58c5ba9431475ca2f715758b136d787eaf1a3614
icmx <32983315+icmx@users.noreply.github.com>
2026-04-02 23:52:27 +0600

    #33 wip setup aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -23,6 +24,11 @@ export default defineConfig({
           ],
         },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
